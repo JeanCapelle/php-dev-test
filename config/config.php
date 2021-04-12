362c6ca706a0cca3088c79ad2bfcbd80cfa5f9c5
JeanCapelle <capelle.jean@gmail.com>
2021-04-12 09:52:53 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,4 +1,4 @@
 <?php
 
-define("OPENWEATHER_API_KEY", "88e3e7ff767a0e786001c0de4054bdc8");
+define("OPENWEATHER_API_KEY", "API KEY");
 define("OPENWEATHER_API_URL", "https://api.openweathermap.org/data/2.5/weather");
